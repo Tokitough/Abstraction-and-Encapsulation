43af17f07a4026db2b08607c9e25d622c3db4291
Kaiser Sajo <jhonkaisersajo@gmail.com>
2023-07-01 17:44:20 +0800

    Accelerate and Brake

--- Car_Class.py
+++ Car_Class.py
@@ -10,22 +10,16 @@ class Car:
     # Add 5 to the speed
     def accelerate (self):
         self.__speed + 5
-        return ("The car is going", self.__speed, "km/h")
+        return self.__speed
     
     # Subtract 5 to the speed
     def brake (self):
-        self.__speed - 5
+        if self.__speed >= 5:
+            self.__speed - 5
+        else: self.__speed = 0
         return self.__speed
     
     # Get current speed
     def get_speed(self):
         return self.__speed
-    
-    # Create a car object
-    def create_car (self):
-        Zoomy = Car(2004, "Toyota", 0)
-        
-        i = 1
-        while i < 6:
-            Zoomy.accelerate()
-            i += 1
+
---
+++ TestCar.py
@@ -0,0 +1,17 @@
+from Car_Class import Car
+
+# Create a car object
+Zoomy = Car(2004, "Toyota", 0)
+
+# Call accelerate 5 times
+print("Accelerating Zoomy")
+for i in range(5):
+    Zoomy.accelerate()
+    print("Zoomy is going", Zoomy.get_speed, "km/h")
+
+# Call brake 5 times
+print("Braking")
+for i in range(5):
+    Zoomy.brake()
+    print("Zoomy is going", Zoomy.get_speed, "km/h")
+    
