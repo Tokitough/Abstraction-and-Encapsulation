2fb50c6bc0e053e9a6fd2bf13b42d8037c0ae9ed
Kaiser Sajo <jhonkaisersajo@gmail.com>
2023-07-01 18:46:29 +0800

    error

--- Pet_Class.py
+++ Pet_Class.py
@@ -2,26 +2,23 @@
 class Pet:
     
     # Pet Attributes
-    def __init__(self, name, animal_type, age):
-        self.__name = name
-        self.__animal_type = animal_type
-        self.__age = age
+    def __init__(self):
+        self.__name = ""
+        self.__animal_type = ""
+        self.__age = 0
     
     # Setter 
     # Set the name of the pet
     def set_name (self, name):
-        self.__name(input("What is the name of your pet?"))
-        return self.__name
+        self.__name = name
     
     # Set the type of animal
     def set_animal_type (self, animal_type):
-        self.__animal_type(input("What kind of animal is your pet?"))
-        return self.__animal_type
+        self.__animal_type = animal_type
     
     # Set the age of the pet
     def set_age (self, age):
-        self.__age(input("How old is your pet?"))
-        return self.__age
+        self.__age = age
     
     # Getter
     #Get the name of the pet
--- TestPet.py
+++ TestPet.py
@@ -1,9 +1,18 @@
 from Pet_Class import Pet
 
+# Create a pet
 pet = Pet()
 
+# Ask user for name of the pet
+name = input("What is the name of your pet?")
 pet.set_name()
+
+# Ask user for the animal type
+animal_type = input("What kind of animal is your pet?")
 pet.set_animal_type()
+
+# Ask user for the age of the pet
+age = input("How old is your pet")
 pet.set_age()
 
 print("\n Pet information:")
