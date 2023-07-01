1ee322b9c66c713f58ac0a14edacc7b3c0476e42
Kaiser Sajo <jhonkaisersajo@gmail.com>
2023-07-01 14:43:35 +0800

    Getter Setter

--- Fan Class.py
+++ Fan Class.py
@@ -9,5 +9,29 @@ class Fan:
         self.__on = on
         self.__radius = radius
         self.__color = color
-        
     
+    # Getter Setter
+    def get_speed(self):
+        return self.__speed
+
+    def set_speed(self, speed):
+        self.__speed = speed
+
+    def is_on(self):
+        return self.__on
+
+    def set_on(self, on):
+        self.__on = on
+
+    def get_radius(self):
+        return self.__radius
+
+    def set_radius(self, radius):
+        self.__radius = radius
+
+    def get_color(self):
+        return self.__color
+
+    def set_color(self, color):
+        self.__color = color
+        
